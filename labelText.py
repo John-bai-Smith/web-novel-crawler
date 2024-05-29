d487c2abfe54cf579be2bb929b8b29bb8c3e6fef
yuuta <zhengmiao2000@outlook.com>
2024-05-29 14:17:54 +0800

    - 优化了labelText的部分代码 - 增加了ebook同步书籍修订记录功能

--- labelText.py
+++ labelText.py
@@ -12,10 +12,9 @@ def add_xml_label(file, file_modified):
     with open(file_modified, 'w', encoding='utf-8') as f:
         for line in lines:
             stripped_line = line.strip()
-            if stripped_line:  # 如果行不为空
-                f.write(f'<p class="calibre1">  {stripped_line}</p>\n')
-            else:
-                f.write(line)  # 如果行为空，原样写回
+            if stripped_line:  # 如果行不为空，可以去除空行
+                # 由于calibre的EPUB编辑器里字符缩进无法用空格或其他字符直接表示，所以暂时用‘\sl’表示缩进，之后在EPUB编辑器里用查找替换将‘\sl’替换为两个字符的缩进
+                f.write(f'<p class="calibre1">\sl{stripped_line}</p>\n')
 
 def add_blank_line(file, file_modified):
     with open(file, 'r', encoding='utf-8') as f:
