65192c7642e60ceb84148f92deb77a022b9858bc
yuuta <23b908108@stu.hit.edu.cn>
2024-06-03 15:45:53 +0800

    - 新增功能：清除垃圾词组后打印已清理的词组及次数

--- erase_word.py
+++ erase_word.py
@@ -17,8 +17,13 @@ with open(file_name, 'r', encoding='utf-8') as f:
 
 # 对每一行进行检查，如果存在需要清除的字符串，将其删除
 with open(file_modified_name, 'w', encoding='utf-8') as f:
+    counts_word = [0] * len(erase_words)
     for line in lines:
-        for word in erase_words:
+        for i, word in enumerate(erase_words):       
+            counts_word[i] = counts_word[i] + line.count(word)
             line = line.replace(word, '')
         if line:
             f.write(line)
+    for i, word in enumerate(erase_words):
+        if counts_word[i] > 0:
+            print(f"{i + 1}: 关键词 '{word}' 检测到 {counts_word[i]} 次")
