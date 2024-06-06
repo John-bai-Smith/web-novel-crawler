90c7dc5682d13079be18da84dcd3b387bc280749
yuuta <23b908108@stu.hit.edu.cn>
2024-06-06 14:59:26 +0800

    - 优化功能：添加xml标签时不再添加自定义的缩进指示符，因为发现EPUB的段落缩进可以在格式文件里统一设置

--- label_text.py
+++ label_text.py
@@ -14,8 +14,8 @@ def add_xml_label(file, file_modified):
         for line in lines:
             stripped_line = line.strip()
             if stripped_line:  # 如果行不为空，可以去除空行
-                # 由于calibre的EPUB编辑器里字符缩进无法用空格或其他字符直接表示，所以暂时用‘\sl’表示缩进，之后在EPUB编辑器里用查找替换将‘\sl’替换为两个字符的缩进
-                f.write(f'<p class="calibre1">\\sl{stripped_line}</p>\n')
+                # EPUB文件的段落缩进可以直接在格式文件里设置
+                f.write(f'<p class="calibre1">{stripped_line}</p>\n')
 
 def add_blank_line(file, file_modified):
     with open(file, 'r', encoding='utf-8') as f:
